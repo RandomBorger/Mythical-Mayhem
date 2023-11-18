3f2c595b1862ad676e509c143646c68ed38f9650
RandomBorger <144975979+RandomBorger@users.noreply.github.com>
2023-11-17 21:53:16 -0700

    Movment Beta

--- LarryScript.cs
+++ LarryScript.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -12,6 +13,7 @@ public class LarryScript : MonoBehaviour
     public float jumpstrenght = 10;
     public float facedir = 0;
     public string direction1 = "Left";
+    public double gravity = 0.0;
   
     // Start is called before the first frame update
     void Start()
@@ -22,14 +24,17 @@ public class LarryScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        myRigidBody.gravityScale = 1;
        string ogdirection = direction1;
        if ((Input.GetKeyDown(KeyCode.A)) == true)
        {
+            myRigidBody.velocity = Vector2.left * jumpstrenght;
             direction1 = "right";
        } else if ((Input.GetKeyDown(KeyCode.D)) == true)
        {
             direction1 = "left";
-       } else
+            myRigidBody.velocity = Vector2.right * jumpstrenght;
+        } else
        {
             facedir = 0;
             Debug.Log("Same Direction");
@@ -37,8 +42,10 @@ public class LarryScript : MonoBehaviour
 
        if (Input.GetKeyDown(KeyCode.Space) == true) 
        {
+            myRigidBody.gravityScale = 2;
             Debug.Log("Jump");
             myRigidBody.velocity = Vector2.up * jumpstrenght;
+
        } 
        if (Input.GetKeyUp(KeyCode.E) == true)
        {
@@ -47,7 +54,7 @@ public class LarryScript : MonoBehaviour
        if (ogdirection !!= direction1)
        {
             facedir = 180; 
-       } 
+       }
         myRigidBody.transform.Rotate(0, facedir, 0);
         facedir = 0;
     }
